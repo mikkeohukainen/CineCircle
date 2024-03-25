f5d419bcea57ef41bdc3c30f19c10a2e2a8663f3
Miku Ohukainen <mikke.ohukainen@gmail.com>
2024-03-25 18:42:22 +0200

    Favorites lisätty

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express();
 
 const users = require("../routes/users");
+const reviews = require('../routes/reviews')
+const favorites = require('../routes/favorites')
+
+app.use(express.json())
 
 app.use("/users", users);
+app.use('/reviews', reviews)
+app.use('/favorites', favorites)
 
 app.get("/", (req, res) => {
   res.status(200).send("hello there");
