2d6eacfba8827112f39cee31ee7ce9a0d1e836a1
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-03-26 12:13:47 +0200

    korjaa uuid-generointi (#13)

--- server/migrations/sqls/20240326080354-groups-owner-id-type-and-foreign-key-up.sql
+++ server/migrations/sqls/20240326080354-groups-owner-id-type-and-foreign-key-up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE groups ALTER COLUMN owner_id TYPE uuid;
+ALTER TABLE groups ALTER COLUMN owner_id TYPE uuid USING (uuid_generate_v4());
 
 ALTER TABLE groups
     ADD FOREIGN KEY (owner_id)
