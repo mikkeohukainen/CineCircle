ccb75ce7fbb45756d9735cb29228df51c30103da
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-04-02 14:35:18 +0300

    käyttäjän rekisteröinti

--- client/src/data/auth.js
+++ client/src/data/auth.js
@@ -1,5 +1,13 @@
 import api from "./api";
 
+export async function register(username, password, confirmPw) {
+  return api.post("/users", {
+    username,
+    password,
+    confirmPw,
+  });
+}
+
 export async function login(username, password) {
   const { data } = await api.post("/users/session", { username, password });
   return data;
