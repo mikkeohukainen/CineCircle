b5ba8bfc08ad8996490386ec3b34039c837dacba
Aki Pyykkönen <aki.pyykkonen@me.com>
2024-04-03 12:28:38 +0300

    ryhmien haku ja listaus toimii

--- client/src/components/GroupInfoCard/GroupInfoCard.jsx
+++ client/src/components/GroupInfoCard/GroupInfoCard.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import classes from "./GroupInfoCard.module.css";
+import {
+  Card,
+  Title,
+  Text,
+  Badge,
+  Button,
+  Group,
+} from "@mantine/core";
 
 export default function GroupCard({
   name,
-  owner,
   description,
-  created_at,
   memberCount,
 }) {
-    return (
-        <div className={classes.infoc}>
-          <h1>{name}</h1>
-          <h2>{description}</h2>
-          <p>
-            Created by: {owner}<br />
-            Created at: {created_at}<br />
-            Members: {memberCount}
-          </p>
-        </div>
-      );
+
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Group justify="space-between" mt="xs" mb="xs">
+        <Title order={1}>{name}</Title>
+        <Badge variant="light" color="gray">
+          Members: {memberCount}
+        </Badge>
+      </Group>
+
+      <Text size="md" c="dimmed">
+        {description}
+      </Text>
+      <Button color="blue" mt="md" radius="md" size="xs">
+        Send Request
+      </Button>
+    </Card>
+  );
 }
--- client/src/pages/GroupsPage/GroupsPage.jsx
+++ client/src/pages/GroupsPage/GroupsPage.jsx
@@ -1,34 +1,66 @@
 import { useEffect, useState } from "react";
-import { Container } from "@mantine/core";
+import { Container, useMantineTheme, Grid, Space, rem } from "@mantine/core";
 import { GroupInfoCard } from "../../components/GroupInfoCard";
+import { SearchBar } from "../../components/SearchBar";
 
 export default function GroupsPage() {
   const [groups, setGroups] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchSubmitted, setSearchSubmitted] = useState(false);
 
   useEffect(() => {
     getGroups();
   }, []);
 
   const getGroups = async () => {
-    const data = await fetch("http://localhost:8000/groups");
-    const groups = await data.json();
-    setGroups(() => groups);
+    const response = await fetch("http://localhost:8000/groups");
+    const groups = await response.json();
+    const sortedGroups = groups.sort((a, b) =>
+      a.group_name.localeCompare(b.group_name)
+    );
+    setGroups(() => sortedGroups);
     console.log("Groups fetched");
-    console.log(groups);
+    console.log(sortedGroups);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchSubmitted(true);
+  };
+
+  const filteredGroups = groups.filter(
+    (group) =>
+      group.group_name.toLowerCase().includes(searchText.toLowerCase()) ||
+      group.description.toLowerCase().includes(searchText.toLowerCase())
+  );
   return (
-    <Container>
-      <h1>Groups</h1>
-      {groups.map((group) => (
-        <GroupInfoCard
-          key={group.group_id}
-          name={group.group_name}
-          description={group.description}
-          created_at={new Date(group.created_at).toLocaleDateString()}
-          owner={group.owner_username}
-          memberCount={group.member_count}
-        />
-      ))}
+    <Container size="xl" mt="lg" mb="xl">
+      <Container size="sm">
+        <form onSubmit={handleSubmit}>
+          <SearchBar
+            placeholder="Search groups by name or description"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              setSearchSubmitted(false);
+            }}
+          />
+          {searchSubmitted && searchText && (
+            <p>Search results for: "{searchText}"</p>
+          )}
+        </form>
+      </Container>
+      <Grid mt="lg" justify="flex-start" align="stretch"gutter="lg">
+        {filteredGroups.map((group) => (
+          <Grid.Col span={6} key={group.group_id}>
+            <GroupInfoCard
+              name={group.group_name}
+              description={group.description}
+              memberCount={group.member_count}
+            />
+          </Grid.Col>
+        ))}
+      </Grid>
     </Container>
   );
 }
