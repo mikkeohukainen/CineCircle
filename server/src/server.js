754a3fb47b18024370220cdda6439fa82c1de4eb
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-04-08 12:12:17 +0300

    Konfiguroitava portti serverille

--- server/src/server.js
+++ server/src/server.js
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
   res.status(200).send("hello there");
 });
 
-app.listen(8000, () => {
-  console.log("Server running on port 8000");
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
 
 // Catch any unhandled errors
