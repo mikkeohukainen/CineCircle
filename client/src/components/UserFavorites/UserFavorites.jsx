87934ea462b36bb421cf7d325be0f97b5e743057
uhoney <t2maju06@students.oamk.fi>
2024-04-08 11:48:44 +0300

    tmp

--- client/src/components/UserFavorites/UserFavorites.jsx
+++ client/src/components/UserFavorites/UserFavorites.jsx
@@ -4,45 +4,37 @@ import { Carousel } from "@mantine/carousel";
 import useAuth from "../../hooks/useAuth";
 import { Button } from "@mantine/core";
 
-// // Tämä vielä pitää kaivaa jostain muualta, nyt on kovakoodattuna
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: import.meta.env.VITE_API_TOKEN,
-  },
-};
-
 export default function UserFavorites() {
   const { username } = useAuth();
 
   const [favorites, setFavorites] = useState(null);
 
   useEffect(() => {
-    const getFavorites = async () => {
-      try {
-        const query = await fetch(`http://localhost:8000/users/${username}/favorites`);
-        const response = await query.json();
-
-        const tmdbData = response.map((favorite) => {
-          if (favorite.type === "movie") {
-            return fetch(`http://api.tmdb.org/3/movie/${favorite.tmdb_id}?language=en-US`, options);
-          } else if (favorite.type === "series") {
-            return fetch(`http://api.tmdb.org/3/tv/${favorite.tmdb_id}?language=en-US`, options);
-          }
-        });
-
-        const tmdbDataResponse = await Promise.all(tmdbData);
-        const movieData = await Promise.all(tmdbDataResponse.map((res) => res.json()));
-
-        setFavorites(movieData);
-        console.log(movieData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
     getFavorites();
-  }, [username]);
+  }, []);
+
+  const getFavorites = async () => {
+    try {
+      const query = await fetch(`http://localhost:8000/users/${username}/favorites`);
+      const response = await query.json();
+
+      const tmdbData = response.map((favorite) => {
+        if (favorite.type === "movie") {
+          return fetch(`http://localhost:8000/search/movie/details/${favorite.tmdb_id}`);
+        } else if (favorite.type === "series") {
+          return fetch(`http://localhost:8000/search/tv/details/${favorite.tmdb_id}`);
+        }
+      });
+
+      const tmdbDataResponse = await Promise.all(tmdbData);
+      const movieData = await Promise.all(tmdbDataResponse.map((res) => res.json()));
+
+      setFavorites(movieData);
+      console.log(movieData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   // If not null, map favorites
   const favoritesSlide = favorites
