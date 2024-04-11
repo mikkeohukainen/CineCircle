ebf188e93cbb9a6f55541f72a6f4648fe2f59198
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-04-11 12:32:23 +0300

    Korjaus tietokannan importteihin

--- server/models/showtimes_model.js
+++ server/models/showtimes_model.js
@@ -1,21 +1,21 @@
-const db = require("../database/db_connection");
+const { dbPool } = require("../database/db_connection");
 
 const showtimes = {
   add: async (showtime) => {
-    await db.query("INSERT INTO showtimes (media_id, theater, showtime) VALUES ($1, $2, $3)", [
+    await dbPool.query("INSERT INTO showtimes (media_id, theater, showtime) VALUES ($1, $2, $3)", [
       showtime.mediaId,
       showtime.theater,
       showtime.showtime,
     ]);
   },
   getAll: async () => {
-    return db.query("SELECT * FROM showtimes");
+    return dbPool.query("SELECT * FROM showtimes");
   },
   getByMediaId: async (mediaId) => {
-    return db.query("SELECT * FROM showtimes WHERE media_id = $1", [mediaId]);
+    return dbPool.query("SELECT * FROM showtimes WHERE media_id = $1", [mediaId]);
   },
   delete: async (id) => {
-    return db.query("DELETE FROM showtimes WHERE id = $1", [id]);
+    return dbPool.query("DELETE FROM showtimes WHERE id = $1", [id]);
   },
 };
 
