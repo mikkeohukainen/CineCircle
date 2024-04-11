165ac198bd7df7efd78327bbb5a3e11065bf4dfd
uhoney <t2maju06@students.oamk.fi>
2024-04-11 20:37:24 +0300

    delete korjausta

--- client/src/components/UserDeleteAccount/UserDeleteAccount.jsx
+++ client/src/components/UserDeleteAccount/UserDeleteAccount.jsx
@@ -4,56 +4,46 @@ import { useForm } from "@mantine/form";
 import { useState } from "react";
 import { logout } from "../../data/auth";
 import { useNavigate } from "react-router-dom";
-import { basicNotification, alterNotification } from "../Notifications";
+import { basicNotification } from "../Notifications";
 
 export default function DeleteAccount() {
   const { username, setIsLoggedIn } = useAuth();
   const navigate = useNavigate();
 
   const form = useForm({
-    initialValues: { username: "", password: "", confirmPw: "", boxChecked: false },
+    initialValues: { password: "", boxChecked: false },
   });
 
   const customNotification = basicNotification();
-  const altNotification = alterNotification();
 
   const handleSubmit = async (values) => {
     if (values.boxChecked === true) {
-      if (values.username === username) {
-        const response = await fetch("http://localhost:8000/users", {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: values.username,
-            password: values.password,
-            confirmPw: values.confirmPw,
-          }),
-        });
+      const response = await fetch("http://localhost:8000/users", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          password: values.password,
+        }),
+      });
 
-        if (!response.ok) {
-          customNotification("Error", "Password does not match confirmation password", "red");
-          // console.error("Failed to delete account");
-        } else {
-          //
-          // TODO: DELETE all data from all tables
-          //
-          customNotification("Ok", "Account permanently deleted", "green");
-          customNotification("Ok", "Logged out", "green");
-          logout();
-          setIsLoggedIn(false);
-          navigate("/");
-          // console.log("Account deleted");
-        }
+      if (!response.ok) {
+        customNotification("Error", "Password incorrect!", "red");
+        // console.error("Failed to delete account");
       } else {
-        customNotification("Error", "Username does not match login", "red");
-        // console.log("You can only delete your own account");
+        //
+        // TODO: DELETE all data from all tables
+        //
+        customNotification("Ok", "Account permanently deleted", "green");
+        customNotification("Ok", "Logged out", "green");
+        logout();
+        setIsLoggedIn(false);
+        navigate("/");
+        // console.log("Account deleted");
       }
     } else {
-      // customNotification("Error", "Please mark the checkbox to confirm deletion", "red");
-      altNotification("Error", "Please mark the checkbox to confirm deletion", "red");
-
+      customNotification("Error", "Please mark the checkbox to confirm deletion", "red");
       // console.log("box not checked");
     }
     form.reset();
@@ -65,20 +55,9 @@ export default function DeleteAccount() {
       <Container size="xs">
         <form onSubmit={form.onSubmit(handleSubmit)}>
           <TextInput
-            label="Username"
-            placeholder="Enter your username"
-            {...form.getInputProps("username")}
-          />
-          <TextInput
             label="Password"
             placeholder="Enter your password"
             {...form.getInputProps("password")}
-            type="password"
-          />
-          <TextInput
-            label="Confirm password"
-            placeholder="Confirm your password"
-            {...form.getInputProps("confirmPw")}
             type="password"
           />
           <Checkbox
