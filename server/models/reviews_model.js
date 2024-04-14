55bdd9df31c5e4082ce1f6ca75da5282fb525e59
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-04-14 15:29:07 +0300

    Korjaa reviews queryt

--- server/models/reviews_model.js
+++ server/models/reviews_model.js
@@ -2,71 +2,39 @@ const { dbPool } = require("../database/db_connection");
 
 const reviews = {
   getAll: async () => {
-    try {
-      const result = await dbPool.query("SELECT * FROM reviews");
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const result = await dbPool.query(
+      "SELECT reviews.*, users.username FROM reviews JOIN users USING (user_id)",
+    );
+    return result.rows;
   },
   getByUsername: async (username) => {
-    try {
-      const result = await dbPool.query(
-        "SELECT * FROM reviews WHERE user_id = (SELECT user_id FROM users WHERE username = $1)",
-        [username],
-      );
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const result = await dbPool.query(
+      "SELECT * FROM reviews JOIN users USING (user_id) WHERE users.username = $1",
+      [username],
+    );
+    return result.rows;
   },
   getByMediaId: async (id) => {
-    try {
-      const result = await dbPool.query("SELECT * FROM reviews WHERE media_id = $1", [id]);
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const result = await dbPool.query("SELECT * FROM reviews WHERE media_id = $1", [id]);
+    return result.rows;
   },
   add: async (body) => {
-    try {
-      const result = await dbPool.query(
-        "INSERT INTO reviews (user_id, media_id, rating, review_text) VALUES ($1, $2, $3, $4) RETURNING *",
-        [body.userId, body.mediaId, body.rating, body.reviewText],
-      );
-      console.log(result.rows);
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const result = await dbPool.query(
+      "INSERT INTO reviews (user_id, media_id, rating, review_text) VALUES ($1, $2, $3, $4) RETURNING *",
+      [body.userId, body.mediaId, body.rating, body.reviewText],
+    );
+    return result.rows;
   },
   update: async (body) => {
-    try {
-      const result = await dbPool.query(
-        "UPDATE reviews SET rating = $1, review_text = $2 WHERE review_id = $3 RETURNING *",
-        [body.rating, body.reviewText, body.reviewId],
-      );
-      console.log(result.rows);
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const result = await dbPool.query(
+      "UPDATE reviews SET rating = $1, review_text = $2 WHERE review_id = $3 RETURNING *",
+      [body.rating, body.reviewText, body.reviewId],
+    );
+    return result.rows;
   },
   delete: async (id) => {
-    try {
-      const result = await dbPool.query("DELETE FROM reviews WHERE review_id = $1 RETURNING *", [
-        id,
-      ]);
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const result = await dbPool.query("DELETE FROM reviews WHERE review_id = $1 RETURNING *", [id]);
+    return result.rows;
   },
 };
 
