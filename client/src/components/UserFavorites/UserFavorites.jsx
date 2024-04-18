dc74d75ae30af56ea1208ab5acd0c34046d442ff
Aki Pyykkönen <aki.pyykkonen@me.com>
2024-04-18 09:49:07 +0300

    lisätty suosikkilistan URL:n kopiointi ja listan näyttö

--- client/src/components/UserFavorites/UserFavorites.jsx
+++ client/src/components/UserFavorites/UserFavorites.jsx
@@ -2,36 +2,39 @@ import { useEffect, useState } from "react";
 import { MovieCard } from "../../components/MovieCard";
 import { Carousel } from "@mantine/carousel";
 import useAuth from "../../hooks/useAuth";
-import { Button } from "@mantine/core";
+import { Button, ActionIcon, Tooltip, rem, Group } from "@mantine/core";
 import { getFavorites } from "../../data/favorites.js";
+import { IconCopy, IconCheck } from "@tabler/icons-react";
+import { showNotification } from "@mantine/notifications";
 
 export default function UserFavorites() {
-  const { username } = useAuth();
-
+  const { username, userId } = useAuth();
   const [favorites, setFavorites] = useState(null);
+  const listId = userId;
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const shareLink = `${baseUrl}/shared-favorites/${listId}`;
 
   useEffect(() => {
     fetchFavorites();
   }, []);
 
-
   const fetchFavorites = async () => {
     try {
       const result = await getFavorites(username);
       // Muutetaan data ennen tilaan tallentamista
-      const modifiedMedia = result.map(media => ({
+      // (MovieCard - komponentti odottaa "media_type", "poster_path" ja "id" - kenttiä)
+      const modifiedMedia = result.map((media) => ({
         ...media,
-        media_type: media.type, // muutetaan "type" -> "media_type"
-        poster_path: media.poster_url, // muutetaan "poster_url" -> "poster_path"
-        id: media.tmdb_id, // muutetaan "tmdb_id" -> "id"
+        media_type: media.type,
+        poster_path: media.poster_url,
+        id: media.tmdb_id,
       }));
-      // sarjat ja leffat erilleen
       setFavorites(modifiedMedia);
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   // If not null, map favorites
   const favoritesSlide = favorites
     ? favorites.map((entry) => (
@@ -41,9 +44,33 @@ export default function UserFavorites() {
       ))
     : null;
 
+  const copyUrlButton = (
+    <Tooltip label="Copy favorites list URL">
+      <ActionIcon
+        size={42}
+        variant="transparent"
+        onClick={() => {
+          navigator.clipboard.writeText(shareLink);
+          showNotification({
+            title: "Copied!",
+            message: "Favorites list URL has been copied to clipboard.",
+            color: "green",
+            icon: <IconCheck />,
+            autoClose: 2000,
+          });
+        }}
+      >
+        <IconCopy style={{ width: rem(24), height: rem(24) }} />
+      </ActionIcon>
+    </Tooltip>
+  );
+
   return (
     <>
-      <h3>Your favorites:</h3>
+      <Group justify="space-between">
+        <h3>Your favorites:</h3>
+        {copyUrlButton}
+      </Group>
       <Carousel
         slideSize={{ base: "33.333%", sm: "20%" }}
         slideGap={{ base: "md", sm: "xl" }}
---
+++ client/src/pages/SharedFavoritesPage/SharedFavoritesPage.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { MovieCard } from "../../components/MovieCard/index.js";
+import { useParams } from "react-router-dom";
+import { Button, Container, Space, Grid, } from "@mantine/core";
+import { getFavoritesByListId } from "../../data/favorites.js";
+
+export default function SharedFavoritesPage() {
+  const { listId } = useParams();
+
+  const [favorites, setFavorites] = useState(null);
+  const [sharedBy, setSharedBy] = useState(null);
+
+  useEffect(() => {
+
+    (async () => {
+      await fetchFavorites();
+    })();
+  }, []);
+
+  const fetchFavorites = async () => {
+    try {
+      const result = await getFavoritesByListId(listId);
+      // Muutetaan data ennen tilaan tallentamista 
+      // (MovieCard - komponentti odottaa "media_type", "poster_path" ja "id" - kenttiä)
+      const modifiedMedia = result.map((media) => ({
+        ...media,
+        media_type: media.type, 
+        poster_path: media.poster_url,
+        id: media.tmdb_id,
+      }));
+      setFavorites(modifiedMedia);
+      setSharedBy(modifiedMedia[0].shared_by);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const favoritesGrid = favorites
+    ? favorites.map((entry) => (
+        <Grid.Col span={{ base: 4, md: 4, lg: 3 }} key={entry.id}>
+          <MovieCard movie={entry} />
+        </Grid.Col>
+      ))
+    : null;
+
+
+  return (
+    <Container size="lg">
+      <h3>{ sharedBy }'s favorites:</h3>
+      <Grid mt="xl">
+        {favoritesGrid}
+      </Grid>
+    </Container>
+  );
+}
